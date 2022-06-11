ba528a81f549783408a4449cb6e67fe471254ab2
MarioMiles <77443888+MarioMiles@users.noreply.github.com>
2022-06-11 11:40:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+const cors = require('cors');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+
+
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/frontend/'}),
