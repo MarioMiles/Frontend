c8a95c4d50402686bad5b9090a1341e3a47996b8
MarioMiles <77443888+MarioMiles@users.noreply.github.com>
2022-06-13 12:50:22 +0200

    Update sugerencias.service.ts

--- src/app/servicios/sugerencias.service.ts
+++ src/app/servicios/sugerencias.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { Note } from '../clases/note';
 import { Sugerencia } from '../clases/sugerencia';
 
-const url = 'http://mariomiles.web23.ovh/sugerencias/'
+const url = 'https://mariomiles.web23.ovh/sugerencias/'
 
 @Injectable({
   providedIn: 'root'
